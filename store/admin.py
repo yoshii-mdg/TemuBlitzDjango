91672d13a94f48bea4aa03f2ee9f853db3092169
MrPopPop <oswasiopongco@gmail.com>
2025-05-18 17:08:53 +0800

    2.0

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,35 @@
-
-# Customer
 from django.contrib import admin
-from .models import *
+from .models import Customer, Product, Order, OrderItem
+from django.db.models import Sum
+from django.utils.timezone import now, timedelta
 
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Order)
 admin.site.register(OrderItem)
 
+
+class OrderAdmin(admin.ModelAdmin):
+    change_list_template = "admin/order_change_list.html"
+
+    def changelist_view(self, request, extra_context=None):
+        today = now().date()
+        labels = []
+        data = []
+
+        for i in range(30):
+            day = today - timedelta(days=i)
+            labels.append(day.strftime('%b %d'))
+            total = Order.objects.filter(date_ordered__date=day).aggregate(Sum('total'))['total__sum'] or 0
+            data.append(float(total))
+
+        labels.reverse()
+        data.reverse()
+
+        extra_context = extra_context or {}
+        extra_context['bar_labels'] = labels
+        extra_context['bar_data'] = data
+
+        return super().changelist_view(request, extra_context=extra_context)
+
+
+admin.site.register(Order, OrderAdmin)
